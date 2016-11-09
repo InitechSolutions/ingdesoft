ad2db80a76cefe7a8ba78252ca3e54d57fab6004
Exequiel Colazo <exequielcolazo@gmail.com>
2016-11-09 20:58:05 -0200

    Agregue id a los favores de seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20161109142629) do
-=======
 ActiveRecord::Schema.define(version: 20161109203810) do
->>>>>>> 47b6900221e88ef10d974f5663d6862cfb785b19
-=======
-ActiveRecord::Schema.define(version: 20161109180436) do
->>>>>>> d20cb3e4c3a9977cf7af4af405b09c3f3835cffb
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,23 +19,9 @@ ActiveRecord::Schema.define(version: 20161109180436) do
     t.string   "titulo"
     t.string   "descripcion"
     t.string   "lugar"
-<<<<<<< HEAD
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-=======
-<<<<<<< HEAD
-    t.string   "titulo"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
-=======
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.date     "fecha"
-    t.integer  "user_id"
-    t.integer  "postulacion_id"
->>>>>>> d20cb3e4c3a9977cf7af4af405b09c3f3835cffb
->>>>>>> 47b6900221e88ef10d974f5663d6862cfb785b19
   end
 
   create_table "logros", force: :cascade do |t|
