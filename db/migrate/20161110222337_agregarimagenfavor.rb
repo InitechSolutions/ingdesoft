bc6bc997e0b5bd8e50ffe8018ca881eb25aceab9
Sofia <basantasofia@gmail.com>
2016-11-11 00:25:56 -0300

    error con migraciones

--- db/migrate/20161110222337_agregarimagenfavor.rb
+++
@@ -1,5 +0,0 @@
-class Agregarimagenfavor < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :favors, :imagen, :string
-  end
-end
--- db/migrate/20161110230210_addimagen.rb
+++
@@ -1,5 +0,0 @@
-class Addimagen < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :users, :imagen, :string
-  end
-end
