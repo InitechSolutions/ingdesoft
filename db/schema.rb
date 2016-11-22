671e9c73105e4d024690a47f99f234b288de3127
Sofia <basantasofia@gmail.com>
2016-11-22 15:44:56 -0300

    commit obligado

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20161112202044) do
     t.string   "lugar"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "imagen"
     t.integer  "user_id"
   end
 
@@ -63,7 +62,6 @@ ActiveRecord::Schema.define(version: 20161112202044) do
     t.boolean  "admin",                  default: false
     t.string   "telefono"
     t.string   "localidad"
-    t.string   "imagen"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
