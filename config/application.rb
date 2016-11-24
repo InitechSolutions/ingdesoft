76a027f85989657df5bfc658e0c77f772527c4a7
Sofia <basantasofia@gmail.com>
2016-11-23 21:24:47 -0300

    precompile fix

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Gauchada2
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  
+
+  config.assets.initialize_on_precompile = false
   end
 end
