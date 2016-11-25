0f72b821ba133154bc8ba56331546ca846302cd9
Tomas Brescia <tomasbrescia@hotmail.com>
2016-11-25 04:00:45 -0300

    y otro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,14 +78,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'mighty-inlet-16686.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 25,
     :domain => "mighty-inlet-16686.herokuapp.com",
-    :user_name => ENV["GMAIL_USERNAME"],
-    :password => ENV["GMAIL_PASSWORD"],
+    :user_name => "solutionsinitech@gmail.com",
+    :password => "initech2016",
     :authentication => "plain",
     :enable_starttls_auto => true
   }
