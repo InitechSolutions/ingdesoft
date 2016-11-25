12357fb0e40c2aa9302661d969c0ca34de9eb8ad
Tomas Brescia <tomasbrescia@hotmail.com>
2016-11-24 21:32:54 -0300

    Reenvio de clave

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: "mighty-inlet-16686.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "solutionsinitech@gmail.com",
