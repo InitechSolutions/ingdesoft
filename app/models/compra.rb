b6e819e600ab47355c36029c405548bf96613e7a
Tomas Brescia <tomasbrescia@hotmail.com>
2016-11-25 05:25:49 -0300

    Validaciones compras arregladas

--- app/models/compra.rb
+++ app/models/compra.rb
@@ -1,10 +1,10 @@
 class Compra < ApplicationRecord
   belongs_to :user
   validates_format_of :name, :with => /\A[^0-9`!@#\$%\^&*+_=]+\z/, :message=>"Nombre incorrecto"
-  validates :puntos, :presence => {:message => "Usted debe ingresar un codigo de seguridad"}
-  validates :puntos, :numericality => { :greater_than => 0, :message => "no pueden ser negativos" }
+  validates :puntos, :presence => {:message => "Debe ingresar la cantidad de puntos a comprar"}
+  validates_numericality_of :puntos, greater_than: 0
   validates_length_of :tarjeta, :is => 16, :message => "El numero de tarjeta es incorrecto"
-  validates :tarjeta, :presence => {:message => "Usted debe ingresar un numero de tarjeta"}
+  validates :tarjeta, :presence => {:message => "Debe ingresar un numero de tarjeta"}
   validates_length_of :code, :is => 3, :message => "El codigo ingresado es incorrecto."
   validate :vencimiento , :vencimiento_mes
 
