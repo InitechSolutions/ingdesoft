9d369f5e113c7a0402de2b863dca9a4b9842e4c3
Sofia <basantasofia@gmail.com>
2016-11-28 21:27:06 -0300

    Vista datos contacto

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
 
   def show
-  	@user = User.find(params[:id])
+    if user_signed_in?
+  	     @user = User.find(params[:id])
+         if current_user.id != @user.id
+           redirect_to (root_path), error: "No tienes permiso"
+         end
+    else
+      redirect_to (root_path), error: "Debes iniciar sesion o registrarte"
+    end
   end
+  # def contacto
+  #   @user = User.find(params[:id])
+  #   if user_signed_in? && (Favor.postulacion_id == @user.id)
+  #     redirect_to (contacto_path)
+  #   else
+  #     redirect_to (root_path), error: "No tienes permiso"
+  #   end
+  # end
+
 end
