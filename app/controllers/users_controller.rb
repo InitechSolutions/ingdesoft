10cfcdab6826e8bd795f04271ca29368ca6fad8e
Sofia <basantasofia@gmail.com>
2016-12-04 23:03:59 -0300

    ver solo perfiles habilitados

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,27 @@ class UsersController < ApplicationController
     if user_signed_in?
   	   @user = User.find(params[:id])
        if current_user.id != @user.id
-         current_user.favors.where(:estado => "procesando").all.each do |favor|
-           if favor.postulations.first.user_id != @user.id
-             redirect_to (user_path(current_user.id)), error: "No tienes permiso"
-             flash[:notice] = "No tenes permiso para ver otro perfil"
+         #El dueño puede ver al postulado elegido
+         if current_user.favors.where(:estado => "procesando").count > 0
+           current_user.favors.where(:estado => "procesando").all.each do |favor|
+             if favor.postulations.first.user_id != @user.id
+               redirect_to (user_path(current_user.id)), error: "No tienes permiso"
+               flash[:notice] = "No tenes permiso para ver otro perfil"
+             end
            end
-         end
-       end
+         #El postulado puede ver al dueño del favor
+         elsif @user.favors.where(:estado => "procesando").count >0
+           @user.favors.where(:estado => "procesando").all.each do |favor|
+             if favor.postulations.first.user_id != current_user.id
+               redirect_to (user_path(current_user.id)), error: "No tienes permiso"
+               flash[:notice] = "No tenes permiso para ver otro perfil"
+             end
+           end
+         else
+           redirect_to (user_path(current_user.id)), error: "No tienes permiso"
+           flash[:notice] = "No tenes permiso para ver otro perfil"
+         end #end if count
+       end #end if current_user.id != @user.id
     else
       redirect_to (root_path), error: "Debes iniciar sesion o registrarte"
       flash[:notice] = "Debes iniciar sesion o registrarte"
